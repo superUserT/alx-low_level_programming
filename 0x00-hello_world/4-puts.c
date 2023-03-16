4a15772641cf7efd00e3551e9314871bb7a8b0c2
superUserT <rantshothabisomail@gmail.com>
2023-03-16 03:00:30 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *  main - Entry is the point
+ *
+ *  Return: Always 0 (Success)
+ *  else the program is broken
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
