37de45a067b81c41134170e2fc84e2e6c9bb0cc6
superUserT <rantshothabisomail@gmail.com>
2023-03-21 07:25:54 -0700

    whitespace

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,32 +1,17 @@
 #include "main.h"
 
-/*main void
- * 
- * return 0
- */
 
 int main(void)
 
 {
-
 _putchar('_');
-
 _putchar('p');
-
 _putchar('u');
-
 _putchar('t');
-
 _putchar('c');
-
 _putchar('h');
-
 _putchar('a');
-
 _putchar('r');
-
 _putchar('\n');
-
 return (0);
-
 }
