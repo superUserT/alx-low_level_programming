2485151aea5583d9ca00e889f9728aa1ddec77a1
superUserT <rantshothabisomail@gmail.com>
2023-03-21 07:22:59 -0700

    doc style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,32 @@
-#include <stdio.h>
 #include "main.h"
 
-/**
-*
-* main - print _putchar 
-*
-* Return 0 
-*
-*/
+/*main void
+ * 
+ * return 0
+ */
 
 int main(void)
+
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+
+_putchar('_');
+
+_putchar('p');
+
+_putchar('u');
+
+_putchar('t');
+
+_putchar('c');
+
+_putchar('h');
+
+_putchar('a');
+
+_putchar('r');
+
+_putchar('\n');
+
+return (0);
+
 }
