5c681911963215a72df9d6ed26f55cc02ce23058
superUserT <rantshothabisomail@gmail.com>
2023-03-21 08:22:10 -0700

    no more

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* c - interger value used to traverse if statement 
-* _islower - checks lowercase or not
-* Return - true or false
+*@c - interger value used to traverse if statement 
+*_islower - checks lowercase or not
+*Return: true or false
 */
 
 int _islower(int c)
