6c06d1dd9ddfc7b74eafb6b15ecb8694677619d6
superUserT <rantshothabisomail@gmail.com>
2023-03-22 02:35:48 -0700

    hot fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,13 +7,19 @@
 
 
 int main(void){
+
 int z;
+
 printf("Infinite loop incoming :(\n");
+
 z = 0;
+
 /*while (z < 10)*/
 /*{*/
 /*putchar(z);*/
 /*}*/
+
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
