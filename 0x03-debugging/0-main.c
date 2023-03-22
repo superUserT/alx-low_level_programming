c5a6ed9ae57d7646ae2693af7b740c042e502684
superUserT <rantshothabisomail@gmail.com>
2023-03-22 02:56:58 -0700

    fixes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * main -Test positivity of a function
- * Return: 0
- */
+* main - Test positivity of a function
+* Return: 0
+*/
 
 int main(void)
 {
+
 int i;
 
 i = 0;
-	
+
 positive_or_negetive(i);
 
 return(0);
