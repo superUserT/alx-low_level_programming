3dcf99fa3cc11ed47ae115fc63c33974a3d291f7
superUserT <rantshothabisomail@gmail.com>
2023-03-22 05:11:22 -0700

    betty?

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,8 @@
 
 /**
 * main - Test function for positive or negative
-* Return: 0
+*
+* Return: Always 0.
 */
 
 int main(void)
@@ -10,6 +11,7 @@ int main(void)
 	int i;
 
 	i = 0;
+
 	positive_or_negetive(i);
 
 	return (0);
