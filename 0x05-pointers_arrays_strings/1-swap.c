fd3711b5bc2b431095e61a3db2dac7fbd53db9b6
superUserT <rantshothabisomail@gmail.com>
2023-03-27 03:50:17 -0700

    my aunt betty is a b**ch

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,16 +2,16 @@
 #include "main.h"
 
 /**
- * swap_int - Swaps two pointers 
+ * swap_int - Swaps two pointers
  * @a: an interger
  * @b: another interger value
  * @c: the silver bullet
- * Return: Void  
+ * Return: Void
  */
 
 void swap_int(int *a, int *b)
 {
-	int c; 
+	int c;
 
 	c = *a;
 	*a = *b;
