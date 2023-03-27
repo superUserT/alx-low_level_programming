411f32b3faf6216bd3f6a65864cae138932be2bb
superUserT <rantshothabisomail@gmail.com>
2023-03-27 03:45:14 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h> "main.h"
+
+/**
+ * swap_int - Swaps two pointers 
+ * @a: an interger
+ * @b: another interger value
+ * @c: the silver bullet
+ * Return: Void  
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c; 
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
