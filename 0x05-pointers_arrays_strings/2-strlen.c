1f9ca98a29ad96ea20b00cc7c0c9052d74949498
superUserT <rantshothabisomail@gmail.com>
2023-03-27 04:40:12 -0700

    woah there buddy

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * strlen - a function that returns the length of a string
+ * _strlen - a function that returns the length of a string
  * @s: a char pointer
  * @character: An interger used to traverse through the char array
  *Return: character to string length
  */
 
-int strlen(char *s)
+int _strlen(char *s)
 {
 	int character  = 0;
 
