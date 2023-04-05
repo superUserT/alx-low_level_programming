0d4e51ac2133d7969950cc9780b310b87650f941
superUserT <rantshothabisomail@gmail.com>
2023-04-05 05:23:27 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_sqrt_recursion - a function that returns the natural square root of a number
+ *@n: an interger
+ *@lr: the lower limit of a recursion
+ *@ur: the upper limit of a recussion
+ *Return 0 for cuccess
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return -1;
+	}
+	else if (n == 0 || n == 1)
+	{
+		return n;
+	}
+	else
+	{
+		int lr = _sqrt_recursion(n/4) * 2;
+		int ur = lr + 1;
+		
+		if (ur * ur > n)
+		{
+			return lr;
+		}
+		else
+		{
+			return ur;
+		}
+	}
+}
+
