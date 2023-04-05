3801d87d94eaea7004347c6ef1085d02276783d7
superUserT <rantshothabisomail@gmail.com>
2023-04-05 05:26:29 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,25 +14,24 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
 	else if (n == 0 || n == 1)
 	{
-		return n;
+		return (n);
 	}
 	else
 	{
-		int lr = _sqrt_recursion(n/4) * 2;
+		int lr = _sqrt_recursion(n / 4) * 2;
 		int ur = lr + 1;
-		
+
 		if (ur * ur > n)
 		{
-			return lr;
+			return (lr);
 		}
 		else
 		{
-			return ur;
+			return (ur);
 		}
 	}
 }
-
