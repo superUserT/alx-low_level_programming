0757239b53b7bb17c2f580c94fb1d369ca062f73
superUserT <rantshothabisomail@gmail.com>
2023-04-05 04:22:12 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,7 +4,11 @@
 #include <string.h>
 
 /*
- *
+ *_print_rev_recursion - a function that prints a string in reverse
+ *@count: a variable used to get the length of a string
+ *@i: an interger used to hold a string 
+ *@s: a pointer charactor
+ *@temp: a temporary variable used to hold the value of an array
  */
 
 void _print_rev_recursion(char *s)
@@ -12,19 +16,19 @@ void _print_rev_recursion(char *s)
 	int count = 0;
 	int i;
 
-	while (*s = '\0')
+	while (*s != '\0')
 	{
 		count++;
 		p++;
-	}/*count here has the length of the string */
-	return count;
+	
 
-	if (i < count / 2)
-	{
-		temp = s[i];
-		s[i] = s[count - i -1];
-		s[count - i - 1] = temp;
-		i++;
-		_print_rev_recursion(s);
+		if (i < count / 2)
+		{
+			temp = s[i];
+			s[i] = s[count - i -1];
+			s[count - i - 1] = temp;
+			i++;
+			_print_rev_recursion(s);
+		}
 	}
 }
