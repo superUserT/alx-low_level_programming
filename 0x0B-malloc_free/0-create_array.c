888bbb9f4af4880ad973485ed7fc2d9067024b31
superUserT <rantshothabisomail@gmail.com>
2023-04-11 03:34:59 -0700

    hot fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,12 @@
 
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
 	char *ptr;
 
 	ptr = malloc(sizeof(char) * size);
 
-	if (size == 0 || size == NULL)
+	if (size == 0 || ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
