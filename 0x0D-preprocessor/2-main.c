b0a94b0aeddf9e859ed2692e450c818fd8c39568
superUserT <rantshothabisomail@gmail.com>
2023-04-17 16:05:59 -0700

    2-main.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file 
+ * Return: zero if succesful
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return 0;
+}
+
