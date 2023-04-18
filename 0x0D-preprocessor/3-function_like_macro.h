581e75f2552497b557bb0a708f8deb04806c637f
superUserT <rantshothabisomail@gmail.com>
2023-04-17 17:08:19 -0700

    end

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef DEV_HEADER_H
+#define DEV_HEADER_H
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef DEV_H
+#define DEV_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
