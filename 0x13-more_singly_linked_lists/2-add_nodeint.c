12d45a37a2b96526258e111397838e3eff11366f
superUserT <rantshothabisomail@gmail.com>
2023-05-03 14:59:46 -0700

    feudal

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,11 +17,10 @@ listint_t *add_nodeint(list_t **head, const int *n)
 	newnode = malloc(sizeof(listint_t));
 
 	if (!newnode)
-	{
 		return (NULL);
-	}
+
 	newnode->n = n;
-	newnode ->next = *head;
+	newnode->next = *head;
 	*head = newnode;
 
 	return (newnode);
