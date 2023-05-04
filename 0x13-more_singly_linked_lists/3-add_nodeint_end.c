680cf94754b24bda1f8f20d2e8421fca9c89320f
superUserT <rantshothabisomail@gmail.com>
2023-05-04 03:27:18 -0700

    addded missing header lists.h

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
+#include "lists.h"
 
-
+/**
+ * add_nodeint_end - adds a node at the end of a linked list
+ * @head: pointer to the first element in the list
+ * @n: data to insert in the new element
+ * Return: pointer to the new node, or NULL if it fails
+ */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
