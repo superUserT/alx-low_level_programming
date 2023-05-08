6d29846bc02e00635ce34ee71653922b80ffa3a7
superUserT <rantshothabisomail@gmail.com>
2023-05-08 14:56:39 -0700

    changed fd

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,9 +2,8 @@
 #include <"stdlib.h>
 #include "main.h"
 
-
 /**
- * read_textfile- Read text file print to STDOUT.
+ * read_textfile - Read text file print to STDOUT.
  * @filename: text file being read
  * @letters: number of letters to be read
  * Return: w- actual number of bytes read and printed
@@ -13,21 +12,21 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
        char *buffer;
-       ssize_t file_descriptor;
+       ssize_t file_desc;
        ssize_t writer;
        ssize_t reader;
 
-       file_descriptor = open(filename, O_RDONLY);
+       file_desc = open(filename, O_RDONLY);
 
-       if  (file_descriptor == -1)
+       if  (file_desc == -1)
        {
 	       return (0);
        }
        buffer = malloc(sizeof(char) * letters);
-       reader = read(file_descriptor, buffer, letters);
+       reader = read(file_desc, buffer, letters);
        writer = write (STDOUT_FILENO, buffer, reader);
 
        free(buffer);
-       close(file_descriptor);
+       close(file_desc);
        return (writer);
 }
