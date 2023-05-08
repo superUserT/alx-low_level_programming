da972f57faea6508e391a20073aba2fd3b98454b
superUserT <rantshothabisomail@gmail.com>
2023-05-08 15:05:12 -0700

    write space

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,30 +3,29 @@
 #include "main.h"
 
 /**
- * read_textfile - Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- */
+* read_textfile - Read text file print to STDOUT.
+* @filename: text file being read
+* @letters: number of letters to be read
+* Return: w- actual number of bytes read and printed
+*/
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-       char *buffer;
-       ssize_t file_desc;
-       ssize_t writer;
-       ssize_t reader;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-       file_desc = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
+	if  (fd == -1)
+	{
+		return (0);
+	}
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-       if  (file_desc == -1)
-       {
-	       return (0);
-       }
-       buffer = malloc(sizeof(char) * letters);
-       reader = read(file_desc, buffer, letters);
-       writer = write (STDOUT_FILENO, buffer, reader);
-
-       free(buffer);
-       close(file_desc);
-       return (writer);
+	free(buf);
+	close(fd);
+	return (w);
 }
