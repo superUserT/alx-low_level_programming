edd3f4cd4b8d766b9eb1057cf1e45805335f3b82
superUserT <rantshothabisomail@gmail.com>
2023-12-12 03:46:51 +0200

    .h file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Libraries */
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/*Prototypes*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
